be3adfba9232f4347afa5340a46c0f9bbc4e7ae5
AmaroLuiz <luizmpoletiamaro@gmail.com>
2026-06-02 08:00:32 -0300

    commit inicia com as estruturas pré definidas

---
+++ src/main/java/br/com/coretech/coretech_api/CoretechApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.coretech.coretech_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoretechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoretechApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/coretech/coretech_api/CoretechApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coretech.coretech_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoretechApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
