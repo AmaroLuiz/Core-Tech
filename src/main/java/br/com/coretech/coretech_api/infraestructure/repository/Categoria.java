25ea87ee2bb2fa05c0732641f03e01e7cc505a67
AmaroLuiz <luizmpoletiamaro@gmail.com>
2026-06-04 19:50:50 -0300

    feat(repository): adiciona produto e categoria no pack de repository

---
+++ src/main/java/br/com/coretech/coretech_api/infraestructure/repository/Categoria.java
@@ -0,0 +1,8 @@
+package br.com.coretech.coretech_api.infraestructure.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Categoria extends JpaRepository<Categoria, Long> {
+}
