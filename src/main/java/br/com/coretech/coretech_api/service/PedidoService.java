bbcadc2facec6eb9f633ba489f9e8c8c8923b160
AmaroLuiz <luizmpoletiamaro@gmail.com>
2026-08-01 22:26:02 -0300

    feat(produto, ProdutoItem): implementa endpoints de produto e pedido item

--- src/main/java/br/com/coretech/coretech_api/service/PedidoService.java
+++ src/main/java/br/com/coretech/coretech_api/service/PedidoService.java
@@ -179,6 +179,35 @@ public class PedidoService {
     }
 
     @Transactional
+    public void apagarPedidoItem(Long id){
+        String email = authService.getUsuarioAutenticadoEmail();
+
+        PedidoItem pedidoItem = pedidoItemRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Item de pedido não encontrado")
+        );
+
+        if (!pedidoItem.getPedido().getUsuario().getEmail().equals(email)){
+            throw new AccessDeniedException("Item de pedido não pertence ao usuario " + email);
+        }
+
+        BigDecimal valor = BigDecimal.ZERO;
+        valor = pedidoItem.getSubTotal();
+
+        Pedido pedido = pedidoItem.getPedido();
+
+
+        pedido.setValorProduto(pedido.getValorProduto().subtract(valor));
+
+        pedido.setValorTotal(pedido.getValorTotal().subtract(valor));
+
+
+        pedidoRepository.save(pedido);
+
+        pedidoItemRepository.delete(pedidoItem);
+
+    }
+
+    @Transactional
     public PedidoResponseDTO atualizaPedido(Long id, PedidoRequestDTO pedidoRequestDTO){
 
         String email = authService.getUsuarioAutenticadoEmail();
@@ -243,32 +272,5 @@ public class PedidoService {
 
     }
 
-    @Transactional
-    public void apagarPedidoItem(Long id){
-        String email = authService.getUsuarioAutenticadoEmail();
 
-        PedidoItem pedidoItem = pedidoItemRepository.findById(id).orElseThrow(
-                () -> new ResourceNotFoundException("Item de pedido não encontrado")
-        );
-
-        if (!pedidoItem.getPedido().getUsuario().getEmail().equals(email)){
-            throw new AccessDeniedException("Item de pedido não pertence ao usuario " + email);
-        }
-
-        BigDecimal valor = BigDecimal.ZERO;
-        valor = pedidoItem.getSubTotal();
-
-        Pedido pedido = pedidoItem.getPedido();
-
-
-        pedido.setValorProduto(pedido.getValorProduto().subtract(valor));
-
-        pedido.setValorTotal(pedido.getValorTotal().subtract(valor));
-
-
-        pedidoRepository.save(pedido);
-
-        pedidoItemRepository.delete(pedidoItem);
-
-    }
 }
